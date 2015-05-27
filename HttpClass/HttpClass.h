ec3a9898d531ca9ff68f142f2f7f257dfd0a3290
程嘉雯 <12163687@qq.com>
2015-05-27 23:59:35 +0800

    Initial Commit

---
+++ HttpClass/HttpClass.h
@@ -0,0 +1,13 @@
+//
+//  HttpClass.h
+//  HttpClass
+//
+//  Created by 程嘉雯 on 15/5/27.
+//  Copyright (c) 2015年 com.epoluodi.lib. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HttpClass : NSObject
+
+@end
---
+++ HttpClass/HttpClass.m
@@ -0,0 +1,13 @@
+//
+//  HttpClass.m
+//  HttpClass
+//
+//  Created by 程嘉雯 on 15/5/27.
+//  Copyright (c) 2015年 com.epoluodi.lib. All rights reserved.
+//
+
+#import "HttpClass.h"
+
+@implementation HttpClass
+
+@end
